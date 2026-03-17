d4cea9b8bc83a4eac1e0f3387e37ed40ae78bec1
manuelalvarezsolt <manuel.alvarez.solt@gmail.com>
2026-03-17 10:17:51 -0300

    Update exercise_currency.py

--- exercise_currency.py
+++ exercise_currency.py
@@ -2,6 +2,6 @@ def currency():
     dolares = 10000/ 1500
     euros = 10000 / 1600
     reales = 10000 / 250
-    print ("la tasa en pesos dolares son", dolares)
-    print ("la tasa en euros son", euros)
-    print ("la tasa en reales son", reales)
+    print (dolares)
+    print (euros)
+    print (reales)
