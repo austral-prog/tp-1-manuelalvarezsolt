adc40c522234d0d54b133de6f638d2875af35c47
manuelalvarezsolt <manuel.alvarez.solt@gmail.com>
2026-03-17 10:11:49 -0300

    Update exercise_age.py

--- exercise_age.py
+++ exercise_age.py
@@ -3,7 +3,7 @@ def age():
     edad_dias = 365 * 25
     edad_horas = 24 * edad_dias
     edad_minutos = 60 * edad_horas
-    print ("la edad en meses es", edad_meses)
+    print (edad_meses)
     print ("la edad en dias es", edad_dias)
     print ("la edad en horas es", edad_horas)
     print ("la edad en minutos es", edad_minutos)
